748d69d862a142b2fd81c1ae114854e0fd683e14
Your Name <github email address>
2019-04-12 15:03:15 -0400

    made random delay slower

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
 
  def random_delay
     puts "sleeping..."
-    sleep (0..2).to_a.sample
+    sleep (0..1).to_a.sample
     puts "awake!"
   end
 
